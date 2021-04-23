4a1625b44380a47fdd07bf6bef60806602a39dab
IBostan <vanyok93@yahoo.com>
2021-04-23 22:32:40 +0300

    #1: Integrate Spring and STOMP

--- src/main/java/com/ibos/config/WebSocketConfig.java
+++ src/main/java/com/ibos/config/WebSocketConfig.java
@@ -8,22 +8,34 @@ import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 import java.util.List;
 
+@Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final HandshakeHandler handshakeHandler;
+
+    public WebSocketConfig(HandshakeHandler handshakeHandler) {
+        this.handshakeHandler = handshakeHandler;
+    }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.setUserDestinationPrefix("/user");
-        config.setApplicationDestinationPrefixes("/app");
-        config.enableSimpleBroker( "/user");
+        config.setApplicationDestinationPrefixes("/antares");
+        config.enableSimpleBroker( "/topic");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setHandshakeHandler(handshakeHandler)
+                .setAllowedOrigins("http://localhost:3000")
                 .withSockJS();
     }
 
--- src/main/java/com/ibos/enums/MessageStatus.java
+++
@@ -1,6 +0,0 @@
-package com.ibos.enums;
-
-public enum MessageStatus {
-    RECEIVED,
-    DELIVERED
-}
